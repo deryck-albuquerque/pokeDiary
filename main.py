9d6bab2488822d8f525cffc061f24dc795de7576
deryck-albuquerque <deryck.henrique22@gmail.com>
2026-04-28 19:11:06 -0400

    chore: update API version to 1.0.0 and improve FastAPI description

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from fastapi import FastAPI
 def init_app() -> FastAPI:
     app = FastAPI(
         title="pokeDiary",
-        description="FastAPI + PostgreSQL",
-        version="initial"
+        description=(
+            "O pokeDiary é uma API backend desenvolvida para gerenciamento de um sistema de diário personalizado de Pokémons. "
+            "Construída com FastAPI, a aplicação segue princípios de Clean Architecture, garantindo escalabilidade, "
+            "manutenibilidade e separação de responsabilidades. O sistema integra PostgreSQL como banco de dados principal, "
+            "RabbitMQ para processamento assíncrono de tarefas e Prisma ORM para modelagem e migrações. "
+            "A aplicação é totalmente containerizada com Docker e preparada para ambientes de produção, incluindo pipelines de CI/CD e deploy em cloud."
+        ),
+        version="1.0.0"
     )
 
     from app.controller import users, auth, diary
